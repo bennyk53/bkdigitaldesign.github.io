799ca2e091961cd06bd6a2727e81e848c9859186
bennyk53 <bennyk.flames@gmail.com>
2020-09-15 18:38:14 -0400

    InitialLoad
    The initial loading of the website

---
+++ js/graphicDesign.js
@@ -0,0 +1,23 @@
+function dtUp(image) {
+	var newImage = "img/graphicDesign/duoTone/titlePicUp.png";
+	image.src = newImage;
+			
+}
+
+function dtDown(image) {
+	var newImage = "img/graphicDesign/duoTone/titlePic.png";
+	image.src = newImage;
+			
+}
+
+function seUp(image) {
+	var newImage = "img/graphicDesign/athletes/titleUp.png";
+	image.src = newImage;
+			
+}
+
+function seDown(image) {
+	var newImage = "img/graphicDesign/athletes/title.png";
+	image.src = newImage;
+			
+}
---
+++ js/index.js
@@ -0,0 +1,35 @@
+function gdUp(image) {
+	var newImage = "img/smokeDomeBKHover.png";
+	image.src = newImage;
+			
+}
+
+function gdDown(image) {
+	var newImage = "img/smokeDomeBK.png";
+	image.src = newImage;
+			
+}
+
+function ldUp(image) {
+	var newImage = "img/bigLogoHover.png";
+	image.src = newImage;
+			
+}
+
+function ldDown(image) {
+	var newImage = "img/bigLogo.png";
+	image.src = newImage;
+			
+}
+
+function webUp(image) {
+	var newImage = "img/webDHover.png";
+	image.src = newImage;
+			
+}
+
+function webDown(image) {
+	var newImage = "img/webD.png";
+	image.src = newImage;
+			
+}
